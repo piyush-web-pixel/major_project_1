e0cac306d3db79348e33f3bd193d9d9a6bb3d704
piyush-web-pixel <piyushyadav6226@gmail.com>
2026-09-19 11:39:02 +0530

    third commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
-const API_URL = "http://127.0.0.1:8000/api/analyze/";
+// const API_URL = "http://127.0.0.1:8000/api/analyze/";
+const API_URL = "/api/analyze/";
 
 
 // =====================================================
