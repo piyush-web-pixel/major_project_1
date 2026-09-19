d6e9d41f4c4b5c4c52dd9816a52223cc406de190
piyush-web-pixel <piyushyadav6226@gmail.com>
2026-09-19 12:01:51 +0530

    Connect frontend to production API

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,8 @@
-// const API_URL = "http://127.0.0.1:8000/api/analyze/";
-const API_URL = "/api/analyze/";
+const API_URL =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1"
+        ? "http://127.0.0.1:8000/api/analyze/"
+        : "https://major-project-1-1-diwh.onrender.com/api/analyze/";
 
 
 // =====================================================
